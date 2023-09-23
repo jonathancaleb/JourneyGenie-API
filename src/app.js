04674a6d8f8072d50e157f08f9746d7f9870ea6d
Caleb <83906743+jonathancaleb@users.noreply.github.com>
2023-09-23 12:46:06 +0300

    #8 Issue- :adhesive_bandage:

--- src/app.js
+++ src/app.js
@@ -1,26 +1,31 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-// const asyncError = require('./middlewares/async_error')
-const errorHandler = require('./middlewares/error_handler');
-require('express-async-errors');
+// Importing necessary modules
+const express = require('express');  // Express.js framework for building web applications
+const morgan = require('morgan');  // HTTP request logger middleware
+const cors = require('cors');  // Cross-Origin Resource Sharing middleware
+const errorHandler = require('./middlewares/error_handler');  // Custom error handling middleware
+require('express-async-errors');  // Middleware for handling async errors in Express
 
-const app = express();
+const app = express();  // Create an instance of the Express application
+
+// Export the Express app instance for usage elsewhere
 exports.app = app;
 
 // Middlewares
+// If the environment is 'dev', use morgan for request logging
 if (process.env.NODE_ENV == 'dev') {
     app.use(morgan('dev'));
 }
 
-// app.use(asyncError());
+// Enable CORS for requests from http://localhost:5000 with credentials
 app.use(cors({
     origin: 'http://localhost:5000',
     credentials: true,
 }));
-app.use(express.json());
+
+app.use(express.json());  // Middleware to parse incoming requests with JSON payloads
 
 // Routes
+// Set up various routes for different API endpoints
 app.use('/api/v1/auth', require('./routes/auth.route'));
 app.use('/api/v1/user', require('./routes/user.route'));
 app.use('/api/v1/vehicle', require('./routes/vehicle.route'));
@@ -32,7 +37,9 @@ app.use('/api/v1/ride', require('./routes/ride.route'));
 app.use('/api/v1/rider', require('./routes/rider.route'));
 
 // Error handler middleware
-app.use(errorHandler);
+app.use(errorHandler);  // Use the custom error handling middleware
+
+// 404 Not Found handler
 app.use((req, res, next) => {
     res.status(404).json({
         status: 'fail',
@@ -40,4 +47,5 @@ app.use((req, res, next) => {
     });
 });
 
+// Export the Express app instance for usage elsewhere
 module.exports = app;
